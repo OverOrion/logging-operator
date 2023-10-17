cd7f43648fefce80d9b1c036b892b25fd0ec5f3e
Szilard Parrag <szilard.parrag@axoflow.com>
2023-10-17 12:52:27 +0200

    gemfile-lock2
    Signed-off-by: Szilard Parrag <szilard.parrag@axoflow.com>

--- pkg/sdk/logging/api/v1beta1/logging_types.go
+++ pkg/sdk/logging/api/v1beta1/logging_types.go
@@ -155,7 +155,7 @@ const (
 	DefaultFluentbitBufferVolumeImageTag        = "v0.7.1"
 	DefaultFluentbitBufferStorageVolumeName     = "fluentbit-buffer"
 	DefaultFluentdImageRepository               = "ghcr.io/overorion/fluentd"
-	DefaultFluentdImageTag                      = "v1.16-gemfile-lock"
+	DefaultFluentdImageTag                      = "v1.16-gemfile-lock2"
 	DefaultFluentdBufferStorageVolumeName       = "fluentd-buffer"
 	DefaultFluentdDrainWatchImageRepository     = "ghcr.io/kube-logging/fluentd-drain-watch"
 	DefaultFluentdDrainWatchImageTag            = "v0.2.1"
